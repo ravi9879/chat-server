efc686383f9360986a875fdf4c8cd269a0e2421e
ravi9879 <144209099+ravi9879@users.noreply.github.com>
2025-06-24 18:28:53 +0530

    Update chatController.js

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -37,7 +37,7 @@ const func = (socket) => {
       // console.log(completion.choices[0].message);
 
       const botMessage = new Message({
-        sender: "ravi",
+        sender: "AI BOT",
         receiver: sender,
         content: completion.choices[0].message.content,
       });
